a636514376f3ba0f2c34d46dad2c53d40ea688f6
yemliha81 <yemlihaddln@hotmail.com>
2025-11-16 10:39:40 +0300

    test 4

--- admin/application/controllers/Product.php
+++ admin/application/controllers/Product.php
@@ -130,6 +130,7 @@ class Product extends CI_Controller {
 	public function update_product_post()
 	{
 	    require DOC_ROOT . 'simpleImage/SimpleImage.php';
+		debug(DOC_ROOT . 'simpleImage/SimpleImage.php');
 		$post = $this->input->post();
 		//debug($post);
 		
